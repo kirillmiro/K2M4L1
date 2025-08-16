673a9e8af577782b9dd37d430b32c5912934305c
kirillmiro <kirill.miro2000@gmail.com>
2025-08-16 22:35:50 +0300

    Kirill

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import schedule
 import threading
 import time
 from config import *
+import os
 
 bot = TeleBot(API_TOKEN)
 
@@ -14,80 +15,16 @@ def gen_markup(id):
     markup.add(InlineKeyboardButton("Получить!", callback_data=id))
     return markup
 
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-
-    prize_id = call.data
-    user_id = call.message.chat.id
-
-    img = manager.get_prize_img(prize_id)
-    with open(f'img/{img}', 'rb') as photo:
-        bot.send_photo(user_id, photo)
-
-
-def send_message():
-    prize_id, img = manager.get_random_prize()[:2]
-    manager.mark_prize_used(prize_id)
-    hide_img(img)
-    for user in manager.get_users():
-        with open(f'hidden_img/{img}', 'rb') as photo:
-            bot.send_photo(user, photo, reply_markup=gen_markup(id = prize_id))
-        
-
-def shedule_thread():
-    schedule.every().minute.do(send_message) # Здесь ты можешь задать периодичность отправки картинок
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    user_id = message.chat.id
-    if user_id in manager.get_users():
-        bot.reply_to(message, "Ты уже зарегестрирован!")
-    else:
-        manager.add_user(user_id, message.from_user.username)
-        bot.reply_to(message, """Привет! Добро пожаловать! 
-Тебя успешно зарегистрировали!
-Каждый час тебе будут приходить новые картинки и у тебя будет шанс их получить!
-Для этого нужно быстрее всех нажать на кнопку 'Получить!'
-Только три первых пользователя получат картинку!)
-
-Комманды:
-/start
-/rating
-/help
-""")
-        
-@bot.message_handler(commands=['help'])
-def handle_start(message):
-    bot.send_message(message.chat.id, """
-Комманды:
-/start
-/rating
-/help!
-""")
-
-        
-@bot.message_handler(commands=['rating'])
-def handle_rating(message):
-    res = manager.get_rating() 
-    res = [f'| @{x[0]:<11} | {x[1]:<11}|\n{"_"*26}' for x in res]
-    res = '\n'.join(res)
-    res = f'|USER_NAME    |COUNT_PRIZE|\n{"_"*26}\n' + res
-    bot.send_message(message.chat.id, res)
-     
-    
-@bot.callback_query_handler(func=lambda call: True)
-def callback_query(call):
-
     prize_id = call.data
     user_id = call.message.chat.id
 
     if manager.get_winners_count(prize_id) < 3:
         res = manager.add_winner(user_id, prize_id)
         if res:
-            img = manager.get_prize_img()
+            img = manager.get_prize_img(prize_id)
             with open(f'img/{img}', 'rb') as photo:
                 bot.send_photo(user_id, photo, caption="Поздравляем! Ты получил картинку!")
         else:
@@ -96,16 +33,101 @@ def callback_query(call):
         bot.send_message(user_id, "К сожалению, ты не успел получить картинку! Попробуй в следующий раз!)")
 
 
+def send_message():
+    prize = manager.get_random_prize()
+    if prize is None:
+        print("Нет доступных призов для отправки.")
+        return
+
+    prize_id, img = prize[:2]
+    manager.mark_prize_used(prize_id)
+    hide_img(img)
+    for user in manager.get_users():
+        with open(f'hidden_img/{img}', 'rb') as photo:
+            bot.send_photo(user, photo, reply_markup=gen_markup(id=prize_id))
+
+        
+
+def shedule_thread():
+    schedule.every().minute.do(send_message)  # периодичность рассылки
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    user_id = message.chat.id
+    if user_id in manager.get_users():
+        bot.reply_to(message, "Ты уже зарегистрирован!")
+    else:
+        manager.add_user(user_id, message.from_user.username)
+        bot.reply_to(message, """Привет! Добро пожаловать! 
+Тебя успешно зарегистрировали!
+Каждый час тебе будут приходить новые картинки и у тебя будет шанс их получить!
+Для этого нужно быстрее всех нажать на кнопку 'Получить!'
+Только три первых пользователя получат картинку!)
+
+Команды:
+/start
+/rating
+/get_my_score
+/help
+""")
+
+
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    bot.send_message(message.chat.id, """
+Команды:
+/start
+/rating
+/get_my_score
+/help
+""")
+
+
+@bot.message_handler(commands=['rating'])
+def handle_rating(message):
+    res = manager.get_rating() 
+    res = [f'| @{x[0]:<11} | {x[1]:<11}|\n{"_"*26}' for x in res]
+    res = '\n'.join(res)
+    res = f'|USER_NAME    |COUNT_PRIZE|\n{"_"*26}\n' + res
+    bot.send_message(message.chat.id, res)
+
+
+@bot.message_handler(commands=['get_my_score'])
+def handle_get_my_score(message):
+    user_id = message.chat.id
+    images = manager.get_winners_img(user_id)
+
+    if not images:
+        bot.send_message(user_id, "У тебя пока нет выигранных картинок 😔")
+        return
+    
+    image_paths = [f"img/{img[0]}" for img in images]
+
+    collage_path = f"collages/collage_{user_id}.jpg"
+    os.makedirs("collages", exist_ok=True)
+    result = create_collage(image_paths, output_path=collage_path)
+
+    if result:
+        with open(result, "rb") as photo:
+            bot.send_photo(user_id, photo, caption="Вот твой коллаж с выигранными картинками 🎉")
+    else:
+        bot.send_message(user_id, "Ошибка при создании коллажа 😥")
+
+
 def polling_thread():
     bot.polling(none_stop=True)
+
 
 if __name__ == '__main__':
     manager = DatabaseManager(DATABASE)
     manager.create_tables()
 
     polling_thread = threading.Thread(target=polling_thread)
-    polling_shedule  = threading.Thread(target=shedule_thread)
+    polling_shedule = threading.Thread(target=shedule_thread)
 
     polling_thread.start()
     polling_shedule.start()
-  
--- logic.py
+++ logic.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from config import DATABASE 
 import os
 import cv2
+from PIL import Image
+
 
 class DatabaseManager:
     def __init__(self, database):
@@ -63,13 +65,11 @@ class DatabaseManager:
                 conn.commit()
                 return 1
 
-  
     def mark_prize_used(self, prize_id):
         conn = sqlite3.connect(self.database)
         with conn:
             conn.execute('''UPDATE prizes SET used = 1 WHERE prize_id = ?''', (prize_id,))
             conn.commit()
-
 
     def get_users(self):
         conn = sqlite3.connect(self.database)
@@ -90,7 +90,11 @@ class DatabaseManager:
         with conn:
             cur = conn.cursor()
             cur.execute("SELECT * FROM prizes WHERE used = 0 ORDER BY random()")
-            return cur.fetchall()[0]
+            prizes = cur.fetchall()
+            if not prizes:
+                return None
+            return prizes[0]
+
         
     def get_winners_count(self, prize_id):
         conn = sqlite3.connect(self.database)
@@ -99,8 +103,6 @@ class DatabaseManager:
             cur.execute('SELECT COUNT(*) FROM winners WHERE prize_id =?', (prize_id, ))
             return cur.fetchall()[0][0]
    
-   
-    
     def get_rating(self):
         conn = sqlite3.connect(self.database)
         with conn:
@@ -113,13 +115,60 @@ class DatabaseManager:
     LIMIT 10
     ''')
             return cur.fetchall()
-  
+
+    def get_winners_img(self, user_id):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute(''' 
+    SELECT image FROM winners 
+    INNER JOIN prizes ON 
+    winners.prize_id = prizes.prize_id
+    WHERE user_id = ?''', (user_id, ))
+            return cur.fetchall()
+
+
 def hide_img(img_name):
     image = cv2.imread(f'img/{img_name}')
     blurred_image = cv2.GaussianBlur(image, (15, 15), 0)
     pixelated_image = cv2.resize(blurred_image, (30, 30), interpolation=cv2.INTER_NEAREST)
     pixelated_image = cv2.resize(pixelated_image, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_NEAREST)
+    os.makedirs("hidden_img", exist_ok=True)
     cv2.imwrite(f'hidden_img/{img_name}', pixelated_image)
+
+
+def create_collage(images, output_path="collage.jpg", collage_size=(500, 500), per_row=3):
+    """
+    Создаёт коллаж из картинок.
+    :param images: список путей к изображениям
+    :param output_path: путь для сохранения результата
+    :param collage_size: размер всего коллажа (ширина, высота)
+    :param per_row: сколько картинок в одном ряду
+    :return: путь к готовому файлу
+    """
+    if not images:
+        return None
+
+    collage_w, collage_h = collage_size
+    rows = (len(images) + per_row - 1) // per_row
+    thumb_w = collage_w // per_row
+    thumb_h = collage_h // rows
+
+    collage = Image.new("RGB", (collage_w, collage_h), color=(255, 255, 255))
+
+    for idx, img_path in enumerate(images):
+        try:
+            img = Image.open(img_path)
+            img = img.resize((thumb_w, thumb_h))
+            x = (idx % per_row) * thumb_w
+            y = (idx // per_row) * thumb_h
+            collage.paste(img, (x, y))
+        except Exception as e:
+            print(f"Ошибка при обработке {img_path}: {e}")
+
+    collage.save(output_path)
+    return output_path
+
 
 if __name__ == '__main__':
     manager = DatabaseManager(DATABASE)
